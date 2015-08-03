cebcb14bb065c1b9f0f335a33e0e96cc00c99ba9
Brian Jopling <brj424@yahoo.com>
2015-08-03 19:46:48 -0400

    Open Button and aesthetics

--- textEditor.py
+++ textEditor.py
@@ -10,29 +10,50 @@ root.focus_set()
 root.geometry("800x600")
 root.configure(background="light blue")
 
-app = Frame(root, background="light blue")
-app.grid()
+frameTop = Frame(root, background="light blue")
+frameTop.pack(side="top")
 
-scrollbar = Scrollbar(app)
+frameBottom = Frame(root, background="light blue")
+frameBottom.pack(side="bottom")
+
+scrollbar = Scrollbar(frameBottom)
 scrollbar.pack(side=RIGHT, fill=Y)
 
-txtPaper = Text(app, wrap="word", yscrollcommand=scrollbar.set, bg="white")
+txtPaper = Text(frameBottom, wrap="word", relief="ridge", yscrollcommand=scrollbar.set, bg="white")
 scrollbar.config(command=txtPaper.yview)
 
-txtFileName = Entry(app)
+lblFileName = Label(frameTop, text="File Name to Open / Save: ", bg="light blue")
+
+txtFileName = Entry(frameTop)
 
 def new():
     txtPaper.delete('1.0', END)
 
+def openf():
+    fileName = txtFileName.get()
+    if fileName == "":
+        lblFileName.config(fg="red")
+        return
+    txt = open(fileName+".txt")
+    new()
+    txtPaper.insert("1.0",txt.read())
+    txtFileName.delete("1.0",END)
+
 def save():
-    f = open(txtFileName.get()+".txt", 'w')
+    fileName = txtFileName.get()
+    if fileName == "":
+        lblFileName.config(fg="red")
+        return
+    lblFileName.config(fg="black")
+    f = open(fileName+".txt", 'w')
     a = txtPaper.get("1.0",END)
     f.write(a)
     f.close()
+    txtFileName.delete("1.0",END)
 
-btnNew = Button(app, text="NEW", command=new, bg="white")
-btnSave = Button(app, text="SAVE", command=save, bg="white")
-lblFileName = Label(app, text="File Name: ", bg="light blue")
+btnNew = Button(frameTop, text="NEW", command=new, bg="white")
+btnOpen = Button(frameTop,text="OPEN", command=openf, bg="white")
+btnSave = Button(frameTop, text="SAVE", command=save, bg="white")
 
 
 def select_all(event):
@@ -44,10 +65,11 @@ def select_all(event):
 txtPaper.bind("<Control-Key-a>", select_all)
 txtPaper.bind("<Control-Key-A>", select_all)
 
-btnNew.pack(side="top")
-btnSave.pack(side="top")
-lblFileName.pack()
-txtFileName.pack()
-txtPaper.pack(side="bottom")
+btnNew.grid(row=1,column=1,pady=(40,0))
+btnOpen.grid(row=1,column=2,padx=(20,0),pady=(40,0))
+btnSave.grid(row=1,column=3,padx=(20,0),pady=(40,0))
+lblFileName.grid(row=1,column=4,padx=(20,0),pady=(40,0))
+txtFileName.grid(row=1,column=5,pady=(40,0))
+txtPaper.pack()
 
 root.mainloop()
